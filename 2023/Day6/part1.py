6544c1e74a7fab56793059396fcdd7217f0e1a98
Rick Schepers <rick.schepers@bigbridge.nl>
2023-12-06 10:29:25 +0100

    improve day 6

--- 2023/Day6/part1.py
+++ 2023/Day6/part1.py
@@ -3,15 +3,13 @@ import re
 with open('input.txt') as f:
     numbers = [int(x) for x in re.findall(r'\d+', f.read())]
 answer = 1
-half_len = int(len(numbers) / 2)
-for i in range(half_len):
+for i in range(len(numbers) // 2):
     time = numbers[i]
-    distance = numbers[i + half_len]
-    possible_wins = 0
+    distance = numbers[i + len(numbers) // 2]
     for offset in range(1, time - 1):
         hold = offset
         travel = time - offset
         if hold * travel > distance:
-            possible_wins += 1
-    answer *= possible_wins
+            answer *= time - (offset * 2 - 1)
+            break
 print(answer)
--- 2023/Day6/part2.py
+++ 2023/Day6/part2.py
@@ -2,13 +2,13 @@ import re
 
 with open('input.txt') as f:
     numbers = [x for x in re.findall(r'\d+', f.read())]
-half_len = int(len(numbers) / 2)
-time = int(''.join(numbers[:half_len]))
-distance = int(''.join(numbers[half_len:]))
-possible_wins = 0
-for offset in range(1, time - 1):
+
+time = int(''.join(numbers[:len(numbers) // 2]))
+distance = int(''.join(numbers[len(numbers) // 2:]))
+
+for offset in range(0, time, 1):
     hold = offset
     travel = time - offset
     if hold * travel > distance:
-        possible_wins += 1
-print(possible_wins)
+        print(time - (offset * 2 - 1))
+        break
