b3a295a2341f785aa0dd202e4287cb16eff7d5de
rtgschepers <schepers.rtg@gmail.com>
2023-12-07 21:41:14 +0100

    finish readme and setup script

--- Readme.md
+++ Readme.md
@@ -1,4 +1,9 @@
+# [Advent of Code](https://adventofcode.com/)
+These are my solutions to the coding puzzles of advent of code.
+All puzzle are done in Python because I like the flexibility and ease of use of this language
+
 ## Setup
+### Run the following commands
 Create a virtualenv
 > python -m venv venv
 
@@ -7,3 +12,18 @@ Activate the virtualenv
 
 Install the requirements
 > pip install -r requirements.txt
+ 
+### Acquire a valid session id
+1. Go to [Advent of Code](https://adventofcode.com/) and open the devtools.
+2. Navigate to Application -> Cookies
+3. Copy the value of the session cookie and paste it in _config.py.tpl_
+4. Remove the _.tpl_ extension, so you're left with _config.py_
+
+## How to use aoc.py
+> python ./aoc.py {year} {day}
+
+This script will generate 2 python files with some code to read the input file.\
+It will also generate an input.txt with your puzzle input for the day, and an empty test.txt for testing purposes.
+
+Example usage
+> python ./aoc.py 2015 1
--- aoc.py
+++ aoc.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import webbrowser
 from pathlib import Path
 
 import requests
@@ -8,17 +9,13 @@ import requests
 def main():
     parser = argparse.ArgumentParser(description='AOC management command.')
 
-    parser.add_argument('operation', help='Create new puzzle files or run a script',
-                        choices=['c', 'r'], metavar='operation')
     parser.add_argument('year', help='Year of the event, e.g. 2015',
                         choices=generate_years(), metavar='year')
     parser.add_argument('day', help='Day of the puzzle, e.g. 5 ',
                         choices=generate_days(), metavar='day')
-    parser.add_argument('part', help='Specify 1 or 2. Only required with operation r',
-                        choices=[1, 2], metavar='part', nargs='?')
 
     args = parser.parse_args()
-    process_data(args.operation, args.year, args.day, args.part)
+    create_new_puzzle_files(args.year, args.day)
 
 
 def generate_years():
@@ -30,14 +27,10 @@ def generate_days():
     return [str(x) for x in range(1, 26)]
 
 
-def process_data(operation, year, day, part):
-    if operation == 'c':
-        create_new_puzzle_files(year, day)
-    elif operation == 'r':
-        return run_puzzle_script(year, day, part)
-
-
 def create_new_puzzle_files(year, day):
+    url = 'https://adventofcode.com/{}/day/{}'.format(year, day)
+    webbrowser.open(url, new=0, autoraise=True)
+
     path = '{}/Day{}'.format(year, day)
     Path(path).mkdir(parents=True, exist_ok=True)
 
@@ -46,7 +39,7 @@ def create_new_puzzle_files(year, day):
         file_path = '{}/part{}.py'.format(path, part)
         if not Path(file_path).is_file():
             with open(file_path, 'w') as f:
-                f.write('for line in [x.rstrip() for x in open(\'input.txt\')]:{0}    break{0}'.format(os.linesep))
+                f.write('for line in [x.rstrip() for x in open(\'test.txt\')]:{0}    break{0}'.format(os.linesep))
 
     file_path = '{}/test.txt'.format(path)
     if not Path(file_path).is_file():
@@ -55,21 +48,19 @@ def create_new_puzzle_files(year, day):
 
     file_path = '{}/input.txt'.format(path)
     if not Path(file_path).is_file():
-        puzzle_input = get_puzzle_input(year, day)
+        puzzle_input = get_puzzle_input(url)
         with open(file_path, 'w') as f:
             f.write(puzzle_input)
 
 
-def run_puzzle_script(year, day, part):
-    return print('run puzzle script')
-
-
-def get_puzzle_input(year, day):
-    url = 'https://adventofcode.com/{}/day/{}/input'.format(year, day)
-    cookies = {}
-    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'}
+def get_puzzle_input(url):
+    url = '{}/input'.format(url)
+    from config import SESSION
+    cookies = {'session': SESSION}
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'}
     response = requests.get(url, cookies=cookies, headers=headers)
-    return response.text
+    return response.text.strip()
 
 
 if __name__ == '__main__':
