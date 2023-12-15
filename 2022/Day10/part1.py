e096488826a2bddfbd4172c675ae2d8eef835721
Rick Schepers <rick.schepers@bigbridge.nl>
2023-12-15 15:28:26 +0100

    2022 day10 part1

--- 2022/Day10/part1.py
+++ 2022/Day10/part1.py
@@ -1,26 +1,29 @@
-cycle = 0
-x = 1
-key_cycles = [20, 60, 100, 140, 180, 220]
+def main():
+    total = 0
+    cycle = 0
+    x = 1
+    for line in [x.rstrip() for x in open('input.txt')]:
+        if 'noop' in line:
+            cycle += 1
+            if is_key_cycle(cycle):
+                total += cycle * x
+        elif 'addx' in line:
+            cycle += 1
+            if is_key_cycle(cycle):
+                total += cycle * x
+            cycle += 1
+            if is_key_cycle(cycle):
+                total += cycle * x
+            value = int(line.split(' ')[1])
+            x += value
+    print(total)
 
 
-def print_answer():
-    print('cycle: {} - x: {}'.format(cycle, x))
+def is_key_cycle(cycle):
+    if (cycle - 20) % 40 == 0:
+        return True
+    return False
 
 
-def increase_cycle():
-    cycle = cycle + 1
-    print_answer()
-
-
-for line in [x.rstrip() for x in open('test.txt')]:
-    cycle += 1
-    parts = line.split(' ')
-
-    print_answer()
-
-    if parts[0] == 'addx':
-        cycle += 1
-        x += int(parts[1])
-        print_answer()
-
-print_answer()
+if __name__ == '__main__':
+    main()
