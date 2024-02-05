198ca11083844656257d550c82f7c260387515f8
Rick Schepers <rick.schepers@bigbridge.nl>
2024-02-05 16:30:02 +0100

    2016 day6

---
+++ 2016/Day6/part1.py
@@ -0,0 +1,23 @@
+from collections import Counter
+
+import numpy as np
+
+
+class Day6:
+
+    def __init__(self):
+        with open('input.txt', 'r') as file:
+            lines = [list(line.strip()) for line in file]
+        self.grid = np.array(lines)
+
+    def solve(self):
+        code = ''
+        for i in range(len(self.grid[0])):
+            col = list(self.grid[:, i])
+            most_common, count = Counter(col).most_common()[0]
+            code += most_common
+        print(code)
+
+
+if __name__ == '__main__':
+    Day6().solve()
