2cfeee8645a7e9d87a066879f0e689b32e442848
rtgschepers <schepers.rtg@gmail.com>
2024-12-09 22:14:56 +0100

    2024 day6 part2 unfinished

--- 2024/Day6/part1.py
+++ 2024/Day6/part1.py
@@ -3,35 +3,41 @@ from timeit import timeit
 
 class Day6:
     grid = []
-    path = []
-    pos = (int, int)
-    direction = (0, -1)
+    start = (int, int, int, int)
     directions = [(0, -1), (1, 0), (0, 1), (-1, 0)]
 
     def __init__(self):
         for line in [x.rstrip() for x in open('input.txt')]:
             if '^' in line:
-                self.pos = (line.index('^'), len(self.grid))
+                self.start = (line.index('^'), len(self.grid), *self.directions[0])
             self.grid.append(list(line))
 
     def solve(self):
-        self.path.append(self.pos)
+        pos = self.start
+        path = [pos]
         while True:
-            next_x, next_y = tuple(a + b for a, b in zip(self.pos, self.direction))
+            x, y, dx, dy = pos
+            next_x, next_y = x + dx, y + dy
             try:
                 next_pos = self.grid[next_y][next_x]
                 if next_pos in '.^':
-                    self.pos = (next_x, next_y)
-                    if self.pos not in self.path:
-                        self.path.append(self.pos)
+                    pos = (next_x, next_y, dx, dy)
+                    if pos not in path:
+                        path.append(pos)
                 elif next_pos == '#':
-                    i = self.directions.index(self.direction)
+                    i = self.directions.index((dx, dy))
                     if i == len(self.directions) - 1:
                         i = -1
-                    self.direction = self.directions[i + 1]
+                    pos = (x, y, *self.directions[i + 1])
             except IndexError:
                 break
-        print(len(self.path))
+
+        unique_path = []
+        for x, y, dx, dy in path:
+            if (x, y) not in unique_path:
+                unique_path.append((x, y))
+        print(len(path))
+        print(len(unique_path))
 
 
 if __name__ == '__main__':
--- 2024/Day6/part2.py
+++ 2024/Day6/part2.py
@@ -1,51 +1,79 @@
 from timeit import timeit
 
+from tqdm import tqdm
+
 
 class Day6:
     grid = []
-    start = (int, int)
-    repeat_count = 10
+    start = (int, int, int, int)
     directions = [(0, -1), (1, 0), (0, 1), (-1, 0)]
 
     def __init__(self):
-        for line in [x.rstrip() for x in open('test.txt')]:
+        for line in [x.rstrip() for x in open('input.txt')]:
             if '^' in line:
-                self.start = (line.index('^'), len(self.grid))
+                self.start = (line.index('^'), len(self.grid), *self.directions[0])
             self.grid.append(list(line))
 
     def solve(self):
+        path = self.get_path()
         loop_count = 0
-        for y in range(len(self.grid)):
-            for x in range(len(self.grid[y])):
-                pos = self.start
-                direction = self.directions[0]
-                temp_grid = [x[:] for x in self.grid]
-                temp_grid[y][x] = '#'
-                path = [pos]
-                repeat = []
+        for path_x, path_y, path_dx, path_dy in tqdm(path):
+            if self.grid[path_y][path_x] != '.':
+                continue
 
-                while True:
-                    next_x, next_y = tuple(a + b for a, b in zip(pos, direction))
-                    try:
-                        next_pos = temp_grid[next_y][next_x]
-                        if next_pos in '.^':
-                            pos = (next_x, next_y)
-                            if pos not in path:
-                                repeat = []
-                                path.append(pos)
-                            elif pos in path and len(repeat) < self.repeat_count:
-                                repeat.append(pos)
-                            elif pos in path and len(repeat) >= self.repeat_count:
-                                loop_count += 1
-                                break
-                        elif next_pos == '#':
-                            i = self.directions.index(direction)
-                            if i == len(self.directions) - 1:
-                                i = -1
-                            direction = self.directions[i + 1]
-                    except IndexError:
-                        break
+            pos = self.start
+            cur_path = [pos]
+
+            temp_grid = [x[:] for x in self.grid]
+            temp_grid[path_y][path_x] = '#'
+
+            while True:
+                x, y, dx, dy = pos
+                next_x, next_y = x + dx, y + dy
+                try:
+                    next_pos = temp_grid[next_y][next_x]
+                    if next_pos in '.^':
+                        pos = (next_x, next_y, dx, dy)
+                        if pos not in cur_path:
+                            cur_path.append(pos)
+                        else:
+                            loop_count += 1
+                            break
+                    elif next_pos == '#':
+                        i = self.directions.index((dx, dy))
+                        if i == len(self.directions) - 1:
+                            i = -1
+                        pos = (x, y, *self.directions[i + 1])
+                except IndexError:
+                    break
         print(loop_count)
+        '''
+        Wrong answers
+        2246
+        2245
+        2247
+        '''
+
+    def get_path(self):
+        pos = self.start
+        path = [pos]
+        while True:
+            x, y, dx, dy = pos
+            next_x, next_y = x + dx, y + dy
+            try:
+                next_pos = self.grid[next_y][next_x]
+                if next_pos in '.^':
+                    pos = (next_x, next_y, dx, dy)
+                    if pos not in path:
+                        path.append(pos)
+                elif next_pos == '#':
+                    i = self.directions.index((dx, dy))
+                    if i == len(self.directions) - 1:
+                        i = -1
+                    pos = (x, y, *self.directions[i + 1])
+            except IndexError:
+                break
+        return path
 
 
 if __name__ == '__main__':
