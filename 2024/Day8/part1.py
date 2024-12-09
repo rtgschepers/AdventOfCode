7f4d59751eceb2a9f8db2800fb93d90a73f34ebb
rtgschepers <schepers.rtg@gmail.com>
2024-12-10 00:56:04 +0100

    2024 day8 complete

--- 2024/Day8/part1.py
+++ 2024/Day8/part1.py
@@ -7,7 +7,7 @@ class Day8:
     layout = {}
 
     def __init__(self):
-        for y, line in enumerate([x.rstrip() for x in open('test.txt')]):
+        for y, line in enumerate([x.rstrip() for x in open('input.txt')]):
             for match in re.finditer(r'((?!\.).)', line):
                 symbol = match.group()
                 if symbol not in self.layout:
@@ -16,7 +16,25 @@ class Day8:
             self.grid.append(line)
 
     def solve(self):
-        pass
+        antinodes = []
+        for x in self.layout.values():
+            for a in range(len(x) - 1):
+                for b in range(a + 1, len(x)):
+                    p1_x, p1_y, p2_x, p2_y = *x[a], *x[b]
+                    dx, dy = p1_x - p2_x, p1_y - p2_y
+
+                    new_pos = self.is_in_bounds(p1_x + dx, p1_y + dy)
+                    if new_pos is not None and new_pos not in antinodes:
+                        antinodes.append(new_pos)
+                    new_pos = self.is_in_bounds(p2_x + dx * -1, p2_y + dy * -1)
+                    if new_pos is not None and new_pos not in antinodes:
+                        antinodes.append(new_pos)
+        print(len(antinodes))
+
+    def is_in_bounds(self, x: int, y: int) -> tuple | None:
+        if 0 <= y < len(self.grid) and 0 <= x < len(self.grid[0]):
+            return x, y
+        return None
 
 
 if __name__ == '__main__':
--- 2024/Day8/part2.py
+++ 2024/Day8/part2.py
@@ -1,13 +1,50 @@
+import re
 from timeit import timeit
 
 
 class Day8:
+    grid = []
+    layout = {}
+
     def __init__(self):
-        for line in [x.rstrip() for x in open('test.txt')]:
-            pass
+        for y, line in enumerate([x.rstrip() for x in open('input.txt')]):
+            for match in re.finditer(r'((?!\.).)', line):
+                symbol = match.group()
+                if symbol not in self.layout:
+                    self.layout[symbol] = []
+                self.layout[symbol].append((match.start(), y))
+            self.grid.append(line)
 
     def solve(self):
-        pass
+        antinodes = []
+        for x in self.layout.values():
+            for a in range(len(x) - 1):
+                for b in range(a + 1, len(x)):
+                    p1_x, p1_y, p2_x, p2_y = *x[a], *x[b]
+                    dx, dy = p1_x - p2_x, p1_y - p2_y
+
+                    new_nodes = self.get_antinodes(p1_x, p1_y, dx, dy)
+                    for node in new_nodes:
+                        if node not in antinodes:
+                            antinodes.append(node)
+
+                    new_nodes = self.get_antinodes(p2_x, p2_y, dx * -1, dy * -1)
+                    for node in new_nodes:
+                        if node not in antinodes:
+                            antinodes.append(node)
+        print(len(antinodes))
+
+    def get_antinodes(self, x: int, y: int, dx: int, dy: int):
+        antinodes = []
+        while self.is_in_bounds(x, y):
+            antinodes.append((x, y))
+            x, y = x + dx, y + dy
+        return antinodes
+
+    def is_in_bounds(self, x: int, y: int) -> tuple | None:
+        if 0 <= y < len(self.grid) and 0 <= x < len(self.grid[0]):
+            return x, y
+        return None
 
 
 if __name__ == '__main__':
