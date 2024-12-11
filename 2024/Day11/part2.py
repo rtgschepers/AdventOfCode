cd62587c971d6ffec2b27050fada05987cbe31e0
Rick Schepers <rick.schepers@bigbridge.nl>
2024-12-11 14:18:25 +0100

    2024 day11 remove cache

--- 2024/Day11/part2.py
+++ 2024/Day11/part2.py
@@ -8,12 +8,10 @@ class Day11:
             self.stones = Counter([int(x) for x in line.split(' ')])
 
     def solve(self):
-        seen_map = {}
-
         for _ in range(75):
             new_stones = {}
             for stone, count in self.stones.items():
-                for m in seen_map.setdefault(stone, self.mutate_value(stone)):
+                for m in self.mutate_value(stone):
                     new_stones[m] = new_stones.get(m, 0) + count
             self.stones = new_stones
         print(sum(self.stones.values()))
